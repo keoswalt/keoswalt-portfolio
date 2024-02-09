56299b38239a88caec9be0874e150936d8f4cb30
Kim Oswalt <130675196+keoswalt@users.noreply.github.com>
2024-02-09 09:15:34 -0600

    Fix favicon

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/main.js');
   eleventyConfig.addPassthroughCopy('./src/assets');
   eleventyConfig.addPassthroughCopy('./src/admin');
+  eleventyConfig.addPassthroughCopy('./src/favicon.svg');
 
   // Return your Object options:
   return {
